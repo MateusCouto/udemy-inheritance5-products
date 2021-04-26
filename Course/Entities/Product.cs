67c42ddaa40bdb97071687b03c19a4c7f9c72390
Mateus Couto <42397879+MateusCouto@users.noreply.github.com>
2021-04-25 22:11:14 -0300

    Adicionar arquivos de projeto.

---
+++ Course/Entities/Product.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course.Entities
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product()
+        {
+        }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public virtual string PriceTag()
+        {
+            StringBuilder sb = new();
+
+            sb.AppendLine("Nome do Produto: " + Name);
+            sb.AppendLine("Produto: R$ " + Price.ToString("F2", CultureInfo.InvariantCulture));
+            return sb.ToString();
+        }
+    }
+}
---
+++ Course/Entities/UsedProduct.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course.Entities
+{
+    class UsedProduct : Product
+    {
+        public DateTime ManufactureDate { get; set; }
+
+        public UsedProduct()
+        {
+        }
+
+        public UsedProduct(string name, double price,DateTime manufacturedate) : base(name, price)
+        {
+            ManufactureDate = manufacturedate;
+        }
+
+        public override string PriceTag()
+        {
+            StringBuilder sb = new();
+
+            sb.AppendLine(base.PriceTag());
+            sb.AppendLine("Manufacture: " + ManufactureDate.ToString("dd/MM/yyyy HH:mm:ss"));
+            sb.AppendLine("Produto (Usado): R$ " + Price.ToString("F2", CultureInfo.InvariantCulture));
+            sb.AppendLine("--------------------------------------------");
+
+            return sb.ToString();
+
+        }
+    }
+}
